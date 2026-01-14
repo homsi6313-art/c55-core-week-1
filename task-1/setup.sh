6b380736b3b65d4a35d5623237585e664982d4d7
homsi6313-art <homsi6313@gmail.com>
2026-01-14 22:02:27 +0100

    Create setup script for new project initialization
    This script sets up a new project by creating necessary directories and files, initializing a git repository, and making initial commits. It also renames a Java file to JavaScript and runs it.

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,67 @@
-# Write your code here
+#!/bin/bash
+set -e
+
+echo "Creating project..."
+
+# Go to the directory where the script is located
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR"
+
+mkdir -p project
+cd project
+
+git init
+
+# Create base files and folders
+touch README.md
+mkdir -p resources
+touch "resources/family picture.jpg"
+touch resources/icon.png
+touch resources/logo.png
+touch settings.conf
+
+mkdir -p src
+touch src/program.java
+mkdir -p src/profile
+mkdir -p src/database
+
+# First commit
+git add .
+git commit -m "initial commit"
+
+sleep 3
+echo "Setup project..."
+
+# Write to README.md
+echo "Welcome to my project" > README.md
+
+# Remove profile folder and family picture
+rm -rf src/profile
+rm -f "resources/family picture.jpg"
+
+# Second commit
+git add .
+git commit -m "Remove profile folder and family picture"
+
+sleep 3
+
+# Show contents of the resources folder
+ls resources
+
+echo "Setup javascript..."
+
+# Rename Java file to JavaScript and write JS code
+mv src/program.java src/program.js
+echo "console.log('JavaScript works!');" > src/program.js
+
+# Run the JavaScript file
+node src/program.js
+
+# Third commit
+git add .
+git commit -m "Setup JavaScript program"
+
+# Show contents of the user's home folder
+ls ~
+
+echo "All done!"
