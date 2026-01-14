8c3c6f4f55a59baa67114fc26deac7ad4f6ec940
homsi6313-art <homsi6313@gmail.com>
2026-01-14 21:06:54 +0100

    Setup project structure and initialize Git
    This script sets up a new project by creating necessary directories and files, initializing a Git repository, and performing initial commits. It also sets up a JavaScript program and cleans up some resources.

--- task-1/setup.sh
+++ task-1/setup.sh
@@ -1,1 +1,50 @@
-# Write your code here
+# Write#!/bin/bash
+
+echo "Creating project..."
+
+mkdir project
+cd project
+
+git init
+
+touch README.md
+mkdir resources
+touch resources/family\ picture.jpg
+touch resources/icon.png
+touch resources/logo.png
+touch settings.conf
+
+mkdir src
+touch src/program.java
+
+git add .
+git commit -m "initial commit"
+
+sleep 3
+
+echo "Setup project..."
+
+echo "Welcome to my project" > README.md
+
+rm -rf profile
+rm resources/family\ picture.jpg
+
+git add .
+git commit -m "Remove profile and family picture"
+
+sleep 3
+
+ls resources
+
+echo "Setup javascript..."
+
+mv src/program.java src/program.js
+echo "console.log('JavaScript works!');" > src/program.js
+
+node src/program.js
+
+git add .
+git commit -m "Setup JavaScript program"
+
+ls ~
+echo "All done!" your code here
